802d609fd1f725c8b77cbcd56d6247f0fdb45873
Shankar-CSE <shankar.l5252@gmail.com>
2025-01-26 14:59:36 +0530

    all the files are moved to the remote repository

---
+++ script.js
@@ -0,0 +1,11 @@
+function resume() {
+    window.open('resume.html', '_blank');
+}
+
+function openResume(){
+    window.open('files/Resume%204.pdf','_blank');
+}
+
+function certificates(){
+    window.open('certificate.html', '_blank');
+}
